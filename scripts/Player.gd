0b10e1c9b0a7000a5225508ba2d67f3af7dbb24a
Robert Shenton <theshenton@gmail.com>
2025-08-02 11:01:11 +0200

    Add boundary walls

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,20 +1,21 @@
 class_name Player
-extends RigidBody3D
+extends CharacterBody3D
 
-const speed:float = 0.075
+const speed:float = 3.0
 const rotateSpeed:float = 0.08
 
-var velocity:float = 0
+var v:float = 0
 
 func _process(_delta: float) -> void:
     if (Input.is_action_pressed("move_forward")):
-        velocity = lerp(velocity, speed, 0.2)
+        v = lerp(v, speed, 0.2)
     else:
-        velocity *= 0.8
+        v *= 0.8
 
     if (Input.is_action_pressed("steer_left")):
         rotate_y(rotateSpeed)
     if (Input.is_action_pressed("steer_right")):
         rotate_y(-rotateSpeed)
 
-    move_and_collide(basis.z * velocity)
+    velocity = velocity.lerp(basis.z * v, 0.2)
+    move_and_slide()

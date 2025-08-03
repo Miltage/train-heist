308ea5a33092f35422581163cc7904b4c3f5e4fc
Robert Shenton <theshenton@gmail.com>
2025-08-03 13:42:53 +0200

    Add arrow to hideout

--- scripts/Hideout.gd
+++ scripts/Hideout.gd
@@ -2,6 +2,7 @@ class_name Hideout
 extends Area3D
 
 var playerOverlapping:bool = false
+var _timeElapsed:float
 
 func _on_body_entered(body:Node3D) -> void:
 	if (body is Player):
@@ -16,3 +17,8 @@ func _input(_event: InputEvent) -> void:
 		Globals.holdingCoin = false
 		Globals.coinsCollected += 1
 		Globals.coin_collected.emit()
+
+func _process(delta: float) -> void:
+	_timeElapsed += delta
+	$arrow.position.z = 2.0 + abs(sin(_timeElapsed * 5)) * 0.25
+	$arrow.visible = Globals.holdingCoin

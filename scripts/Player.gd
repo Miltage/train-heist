f8e31b2b35a52e2014ca2c06cefa258ce829a538
Robert Shenton <theshenton@gmail.com>
2025-08-03 11:44:16 +0200

    Polish for player on horse

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const speed:float = 3.0
 const rotateSpeed:float = 0.08
+const velThreshold:float = 0.1
 
 var v:float = 0
 var boarded:bool = false : set = set_boarded
@@ -12,15 +13,26 @@ var targetRotation:Vector3
 
 var _timeElapsed:float
 var _bounce:float
+var _run:float
+var _anim:bool
 
 func _process(delta: float) -> void:
 	_timeElapsed += delta
+	_run += delta
+
+	if (_run > 0.2):
+		_run = 0.0
+		_anim = !_anim
 
 	$player/Hands.visible = !boarded && !Globals.holdingCoin
 	$player/HandsCoin.visible = !boarded && Globals.holdingCoin
 	$player/Bandit.visible = !boarded
+	$player/Horse.visible = !_anim || !is_running()
+	$player/Horse2.visible = _anim && is_running()
 
-	var s = 0.2 if (velocity.length() > 1.0 || boarded) else 0.0
+	position.y = 0.1 if _anim && is_running() else 0.0
+
+	var s = 0.1 if (is_running()) else 0.0
 	_bounce = lerp(_bounce, s, 0.2)
 	scale = Vector3.ONE * 1.0 + Vector3.ONE * abs(sin(_timeElapsed * 10)) * _bounce
 
@@ -42,8 +54,13 @@ func _process(delta: float) -> void:
 	move_and_slide()
 
 func follow_car() -> void:
-	position = followingCar.global_position + followingCar.basis.x * -1.65
+	var newPos = followingCar.global_position + followingCar.basis.x * -1.65
+	velocity = newPos - position
+	position = newPos
 	look_at(followingCar.global_position)
 
 func set_boarded(newBoarded:bool) -> void:
 	boarded = newBoarded
+
+func is_running() -> bool:
+	return velocity.length() > velThreshold || boarded
